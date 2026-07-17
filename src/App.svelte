f9e64a210bf9e5378c44cd2d82e36004b7ba2ad6
EpicDima <dima2-8@bk.ru>
2026-07-17 05:24:26 +0300

    fix: update document title reactively on language change

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,11 @@
   import Sidebar from './lib/widgets/Sidebar.svelte';
   import ThemeToggle from './lib/widgets/ThemeToggle.svelte';
   import { appState } from './lib/shared/stores/app.svelte.js';
+  import { t } from './lib/shared/i18n/index.svelte.js';
+
+  $effect(() => {
+    document.title = t('appTitle');
+  });
 </script>
 
 <div class="relative w-full h-full" class:dark={appState.darkMode}>
